a457567c8fe78075c9d4ffce3268ad5a9c055933
konradbolek <konrad.bolek@polcode.net>
2017-05-15 21:50:44 +0200

    Underscore with gulp, FA, Bootstrap V1.0

--- header.php
+++ header.php
@@ -22,8 +22,9 @@
 
 <body <?php body_class(); ?>>
 <div id="page" class="site">
+	<a class="skip-link screen-reader-text" href="#main"><?php esc_html_e( 'Skip to content', 'gulp-wordpress' ); ?></a>
 
-	<header id="masthead" class="site-header" role="banner"><i class="fa fa-address-book-o" aria-hidden="true"></i>
+	<header id="masthead" class="site-header" role="banner">
 		<div class="site-branding">
 			<?php
 			if ( is_front_page() && is_home() ) : ?>
